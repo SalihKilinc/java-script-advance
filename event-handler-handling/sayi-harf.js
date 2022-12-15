23b571f3cb1cdf027ee95235e11e2a26729e2708
salih <salihkilinc80@gmail.com>
2022-12-15 22:04:41 +0100

    w1

---
+++ event-handler-handling/sayi-harf.js
@@ -0,0 +1,18 @@
+const score1El = document.querySelector("#txtScore1")
+const score2El = document.querySelector("#txtScore2")
+const score3El = document.querySelector("#txtScore3")
+const btnAverage = document.querySelector("#btnAverage")
+
+btnAverage.addEventListener("click" , () =>{
+
+})
+
+const calculateAverage = (num1 , num2 , num3) => {
+    // ortalama islemi
+    return 15;
+}
+
+const convertScoreLetter = (num) => {
+    //harfe cevirme islemi
+    return "A";
+}
