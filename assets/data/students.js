292411c6aff669da3eb77b2942a3805bf9c0a671
salih <salihkilinc80@gmail.com>
2022-12-21 18:10:42 +0100

    f1

---
+++ assets/data/students.js
@@ -0,0 +1,6 @@
+export const students = [
+    {id: 1, name :"Mehmet" , point :85},
+    {id: 2, name :"Selma" , point :90},
+    {id: 3, name :"Semra" , point :95},
+    {id: 4, name :"Kenan" , point :100},
+]
