6b426e8f93f48066242dfbd893bf1e6754886138
salih <salihkilinc80@gmail.com>
2022-12-22 21:56:29 +0100

    j9

---
+++ assets/js/practise-setinterval.js
@@ -0,0 +1,15 @@
+const hourEl = document.querySelector("#clock li:nth-child(1)")
+const minuteEl = document.querySelector("#clock li:nth-child(3)")
+const secondEl = document.querySelector("#clock li:nth-child(2)")
+
+
+setInterval (() =>{
+    const dateTime= new Date ();
+    const hour = dateTime.getHours();
+    const minute = dateTime.getMinutes();
+
+    hourEl.innerHTML=hour;
+    minuteEl.innerText=minute;
+    secondEl.classList.toggle("hidden") // saniyenin yanip sonmesini yaptik
+
+},1000)
