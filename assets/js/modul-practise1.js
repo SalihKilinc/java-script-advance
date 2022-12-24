861809722ec0fb29c70eb51f2b48ae5dae95309b
salih <salihkilinc80@gmail.com>
2022-12-24 21:33:46 +0100

    3

--- assets/js/modul-practise1.js
+++ assets/js/modul-practise1.js
@@ -1,14 +1,24 @@
-import { sSortASC } from "./modules/practise.js";
+import { sSortASC ,sSortDESC ,nSortASC } from "./modules/practise.js";
 
+const arr = ["Ali", "Veli", "Şeyma", "Çiçek", "Bülent"];
+const arr2 =[12,21,23,13,31,25,22]
 document.getElementById("btn1").addEventListener("click", ()=>{
-    const arr = ["Ali", "Veli", "Şeyma", "Çiçek", "Bülent"];
+   
     const newArr = sSortASC(arr);
     console.log(arr);
     console.log(newArr);
 });
 document.getElementById("btn2").addEventListener("click", ()=>{
+   
+    const newArr = sSortDESC(arr);
+    console.log(arr);
+    console.log(newArr);
 });
 document.getElementById("btn3").addEventListener("click", ()=>{
+    
+    const newArr = nSortASC(arr2);
+    console.log(arr2);
+    console.log(newArr);
 });
 document.getElementById("btn4").addEventListener("click", ()=>{
 });
--- assets/js/modules/practise.js
+++ assets/js/modules/practise.js
@@ -1,8 +1,27 @@
 
 const sSortASC = (arr) => {
     const sortedArr = [...arr].sort( (a,b) => {
-        console.log(a,b)
+       // console.log(a,b,a.localeCompare (b))
+        return a.localeCompare (b)
     }  )
     return sortedArr;
 }
-export {sSortASC}
+
+const sSortDESC = (arr) => {
+    const sortedArr = [...arr].sort( (a,b) => {
+       // console.log(a,b,b.localeCompare (a))
+        return a.localeCompare (b)
+    }  )
+    return sortedArr;
+}
+
+const nSortASC = (arr) => {
+    const sortedArr = [...arr].sort( (a,b) => {
+        console.log(a,b ,a-b)
+        return a-b;
+})
+return sortedArr;
+}
+
+
+export {sSortASC , sSortDESC ,nSortASC}
